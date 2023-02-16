4ef593a8955ce47d6eedee6c60229a232a90c830
Timothy Ransom <tsranso@login003.palmetto.clemson.edu>
2023-02-16 08:38:34 -0500

    an ontology of version control

---
+++ globals.R
@@ -0,0 +1,24 @@
+# ---- subs-of-interest ----
+subs_of_interest <- c("learnpython",
+                      "Python",
+                      "learnprogramming",
+                      "programming",
+                      "AskProgramming",
+                      "CSEducation",
+                      "computerscience",
+                      "AskComputerScience",
+                      "compsci",
+                      "AskCompSci",
+                      "cscareerquestions",
+                      "coding",
+                      "javascript",
+                      "learnjavascript",
+                      "java",
+                      "learnjava",
+                      "haskell",
+                      "haskellquestions",
+                      "C_Programming")
+
+# ---- years-of-interest ----
+years <- c(2014, 2015, 2016, 2017)
+
